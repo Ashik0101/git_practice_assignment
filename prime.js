deea91ec0f3c63b1542431db67377947c86cb6d5
aslammmahetar <115460260+aslammmahetar@users.noreply.github.com>
2022-11-03 22:37:35 +0530

    prime code fixed

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 function checkPrime(num){
     let count = 0;
-    for(let i=1;i<num;i++){
-        if(num % 2==0){
+    for(let i=1;i<=num;i++){
+        if(num % i==0){
             count++;
         }
     }
-    if(count == i){
+    if(count == 2){
         console.log(num , "is Prime Number");
     }else{
         console.log(num, "is not a Prime Number");
